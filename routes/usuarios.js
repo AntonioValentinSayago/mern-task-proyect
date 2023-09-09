dedbb5ed54f137926c4ea2a601f306faa578319f
AntonioValentin <cesar_anto30@hotmail.com>
2023-09-09 17:18:42 -0600

    Middleware is added to validate the fields

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -6,22 +6,25 @@ const { usuariosGet,
         usuariosPost,
         usuariosDelete,
         usuariosPatch } = require('../controllers/usuarios');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
-
 
 router.get('/', usuariosGet );
 
 router.put('/:id', usuariosPut );
 
-router.post('/', usuariosPost );
+router.post('/', [
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'El password debe ser más de 6 letras').isLength({ min: 6 }),
+    check('correo', 'El correo no es válido').isEmail(),
+    check('rol', 'No es un rol válido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    validarCampos
+], usuariosPost );
 
 router.delete('/', usuariosDelete );
 
 router.patch('/', usuariosPatch );
-
-
-
-
 
 module.exports = router;
