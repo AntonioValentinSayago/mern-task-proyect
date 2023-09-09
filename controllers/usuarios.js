bf17a43bca6dba04c10d6c8354713dd6cff68bd7
AntonioValentin <cesar_anto30@hotmail.com>
2023-09-09 17:31:20 -0600

    Validation for the email to be unique

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,5 +1,7 @@
 const { response, request } = require('express');
+const bcryptjs = require('bcryptjs');
 
+const Usuario = require('../models/usuario');
 
 const usuariosGet = (req = request, res = response) => {
 
@@ -15,14 +17,29 @@ const usuariosGet = (req = request, res = response) => {
     });
 }
 
-const usuariosPost = (req, res = response) => {
+const usuariosPost = async(req, res = response) => {
 
-    const { nombre, edad } = req.body;
+    const { nombre, correo, password, rol } = req.body;
+    const usuario = new Usuario( { nombre, correo, password, rol } )
+
+    //Verificar si el correo existe || Valudacion del correo
+    const existeEmail = await Usuario.findOne( { correo })
+    if ( existeEmail ) {
+        return res.status( 400 ).json( {
+            msg: ' El correo ya está registrado'
+        })
+    }
+
+    // Encripto	 la contraseña
+    const salt = bcryptjs.genSaltSync()
+    usuario.password = bcryptjs.hashSync( password, salt )
+
+    //Guardar los datos en la base de datos
+    await usuario.save();
 
     res.json({
         msg: 'post API - usuariosPost',
-        nombre, 
-        edad
+        usuario
     });
 }
 
@@ -47,9 +64,6 @@ const usuariosDelete = (req, res = response) => {
         msg: 'delete API - usuariosDelete'
     });
 }
-
-
-
 
 module.exports = {
     usuariosGet,
