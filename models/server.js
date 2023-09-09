7a55ed630fedca17b14e50f3146d596118a14661
AntonioValentin <cesar_anto30@hotmail.com>
2023-09-08 21:00:49 -0600

    INstacia para el metodo conexion

--- models/server.js
+++ models/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { dbConnection } = require('../database/config');
 
 class Server {
 
@@ -8,11 +9,17 @@ class Server {
         this.port = process.env.PORT;
         this.usuariosPath = '/api/usuarios';
 
+        // Conectar a base de datos 
+        this.conectarDB();
         // Middlewares
         this.middlewares();
 
         // Rutas de mi aplicación
         this.routes();
+    }
+
+    async conectarDB(){
+        await dbConnection()
     }
 
     middlewares() {
