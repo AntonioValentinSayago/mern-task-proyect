6f1164a2d2623d7eeaf40c05726d90ec7d6d04cd
AntonioValentin <cesar_anto30@hotmail.com>
2023-09-10 21:15:08 -0600

    Creacion del schema para extraer los rols de la DB

---
+++ models/role.js
@@ -0,0 +1,11 @@
+
+const { Schema, model } = require('mongoose')
+
+const RoleSchema = Schema({
+    rol:{
+        type: String,
+        required: [true, ' El rol es Obligatorio']
+    }
+})
+
+module.exports = model( 'Role', RoleSchema)
