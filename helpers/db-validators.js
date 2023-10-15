56bea52219e1b1950fa6e1437149a6e6468602f5
AntonioValentin <cesar_anto30@hotmail.com>
2023-10-14 21:21:26 -0600

    Implementando las funciones para validar el Email

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -1,5 +1,7 @@
 const Role = require('../models/role');
 
+const Usuario = require('../models/usuario');
+
 const esRoleValido = async( rol = '') => {
     const existeRol = await Role.findOne( { rol })
     if ( !existeRol ) {
@@ -7,7 +9,27 @@ const esRoleValido = async( rol = '') => {
     }
 }
 
+const emailExiste = async( email = '' ) => {
+
+    // ? Verificar si el correo existe || Valudacion del correo
+    const existeEmail = await Usuario.findOne( { correo })
+    if ( existeEmail ) {
+        throw new Error( 'El correo ya esta registrado' )
+    }
+}
+
+const existeUsuarioPorId = async ( id ) => {
+
+    // ? Verificar si el correo existe
+    const existeUsuario = await Usuario.findId( id );
+    if ( !existeUsuario ) {
+        throw new Error( 'El ID no existe' )
+    }
+}
+
 
 module.exports = {
-    esRoleValido
+    esRoleValido,
+    emailExiste,
+    existeUsuarioPorId
 }
