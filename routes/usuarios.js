670e013513df6bb7a7f79cd845c0ff533e9e2235
AntonioValentin <cesar_anto30@hotmail.com>
2023-10-14 21:20:46 -0600

    Configurando las rutas de Middlewares

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -9,18 +9,24 @@ const { usuariosGet,
 
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
-const { esRoleValido } = require('../helpers/db-validators');
+const { esRoleValido, existeUsuarioPorId } = require('../helpers/db-validators');
 
 const router = Router();
 
 router.get('/', usuariosGet );
 
-router.put('/:id', usuariosPut );
+router.put('/:id',[
+    check('id', 'No es un ID válido').isMongoId(),
+    check('id ').custom( existeUsuarioPorId ),
+    check('rol').custom( esRoleValido ),
+    validarCampos
+], usuariosPut );
 
 router.post('/', [
     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
     check('password', 'El password debe ser más de 6 letras').isLength({ min: 6 }),
-    check('correo', 'El correo no es válido').isEmail(),
+    //check('correo', 'El correo no es válido').isEmail(),
+    //check('correo').custom( emailExiste ),
     //check('rol', 'No es un rol válido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
     check('rol').custom( esRoleValido ),
     validarCampos
