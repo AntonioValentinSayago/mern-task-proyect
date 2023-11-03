d5a397c59d98eb6ab3a58f9b2dbb4744975b46ea
AntonioValentin <cesar_anto30@hotmail.com>
2023-11-03 11:31:47 -0600

    Insertando datos a MongooseDB, en body-parse

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -1,7 +1,25 @@
 //? Controlador para el intermediario entre el modelo usuario y la vista 
+import Usuario from "../models/Usuario.js"
 
-const registrar = ( req, res) => {
-    res.send("Desde API/USUARIOS")
+const registrar = async ( req, res) => {
+
+    // * Evitar registros duplicados
+    const { email } = req.body;
+    const existeUsuario = await Usuario.findOne({ email })
+
+    if(existeUsuario) {
+        const error = new Error('Usuario ya Registrado')
+        return res.status(400).json({ msg: error.message })
+    }
+
+    try {
+        const usuario = new Usuario(req.body);
+        const usuarioAlmacenado = await usuario.save();
+        res.send(usuarioAlmacenado)
+    } catch (error) {
+        console.log(error)
+    }
+
 }
 
 export {
