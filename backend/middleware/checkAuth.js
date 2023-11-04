291d031c41be065ae0d896d943b214b404ac0035
AntonioValentin <cesar_anto30@hotmail.com>
2023-11-04 16:27:46 -0600

    Validacion para detectar el usuario Conectado

--- backend/middleware/checkAuth.js
+++ backend/middleware/checkAuth.js
@@ -19,7 +19,7 @@ const checkAuth = async ( req, res, next) => {
 
     if (!token) {
         const error = new Error('El Token no es Valido')
-        res.status(401).json({ msg: error.message })
+        return res.status(401).json({ msg: error.message })
     }
 
     next();
