10dc2a32b6547b6bf70614e0e4d85c98e5e008f4
AntonioValentin <cesar_anto30@hotmail.com>
2023-11-04 16:28:37 -0600

    Primeros Pasos, para la configuracion del CRUD de proyectos

---
+++ backend/controllers/proyectoController.js
@@ -0,0 +1,52 @@
+import Proyecto from "../models/Proyecto.js"
+
+const obtenerProyectos = async ( req, res ) => {
+
+    const proyectos = await Proyecto.find().where('creador').equals(req.usuario);
+    res.json(proyectos)
+
+}
+
+const nuevoProyecto = async ( req, res ) => {
+
+    const proyecto = new Proyecto(req.body);
+    proyecto.creador = req.usuario._id;
+
+    try {
+        const proyectoAlmacenado = await proyecto.save();
+        res.json(proyectoAlmacenado)
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+const obtenerProyecto = async ( req, res ) => {}
+const editarProyecto = async ( req, res ) => {
+
+}
+const eliminarProyecto = async ( req, res ) => {
+
+}
+const agregarColaborador = async ( req, res ) => {
+
+}
+const eliminarColaborador = async ( req, res ) => {
+
+}
+const obtenerTareas = async ( req, res ) => {
+
+}
+
+export{
+    obtenerProyectos,
+    nuevoProyecto,
+    obtenerProyecto,
+    editarProyecto,
+    eliminarProyecto,
+    agregarColaborador,
+    eliminarColaborador,
+    obtenerTareas
+}
+
+
+
---
+++ backend/models/Proyecto.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const proyectosShema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    descripcion:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    fechaEntrega:{
+        type: Date,
+        default: Date.now(),
+    },
+    cliente: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    creador:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Usuario"
+    },
+    colaboradores:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Usuario"
+        },
+    ],
+},{
+    timestamps: true
+});
+
+const Proyecto = mongoose.model('Proyecto', proyectosShema)
+
+export default Proyecto;
