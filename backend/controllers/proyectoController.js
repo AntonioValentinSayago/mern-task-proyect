bf5d8afa6d0988f64047255e6a4657b230209340
AntonioValentin <cesar_anto30@hotmail.com>
2023-11-04 17:16:13 -0600

    Funcion para Editar y Eliminar Proyectos

--- backend/controllers/proyectoController.js
+++ backend/controllers/proyectoController.js
@@ -1,13 +1,13 @@
 import Proyecto from "../models/Proyecto.js"
 
-const obtenerProyectos = async ( req, res ) => {
+const obtenerProyectos = async (req, res) => {
 
     const proyectos = await Proyecto.find().where('creador').equals(req.usuario);
     res.json(proyectos)
 
 }
 
-const nuevoProyecto = async ( req, res ) => {
+const nuevoProyecto = async (req, res) => {
 
     const proyecto = new Proyecto(req.body);
     proyecto.creador = req.usuario._id;
@@ -22,20 +22,20 @@ const nuevoProyecto = async ( req, res ) => {
 }
 
 // * Obtener proyecto existente desde la url
-const obtenerProyecto = async ( req, res ) => {
+const obtenerProyecto = async (req, res) => {
 
     const { id } = req.params;
 
     const proyecto = await Proyecto.findById(id.trim());
 
     // ? En caso de no exista el proyecto se muestra un msg
-    if(!proyecto && id.length === 24){
+    if (!proyecto && id.length === 24) {
         const error = new Error('El Proyecto no Existe')
         return res.status(404).json({ msg: error.message })
     }
-    
+
     // ? Cuando el usuario no tiene permisos para ver el proyecto
-    if(proyecto.creador.toString() !== req.usuario._id.toString()){
+    if (proyecto.creador.toString() !== req.usuario._id.toString()) {
         const error = new Error('Acción no Valida')
         return res.status(401).json({ msg: error.message })
     }
@@ -43,13 +43,72 @@ const obtenerProyecto = async ( req, res ) => {
     res.json(proyecto)
 
 }
-const editarProyecto = async ( req, res ) => {}
-const eliminarProyecto = async ( req, res ) => {}
-const agregarColaborador = async ( req, res ) => {}
-const eliminarColaborador = async ( req, res ) => {}
-const obtenerTareas = async ( req, res ) => {}
 
-export{
+// * Funcion para Editar un Proyecto
+const editarProyecto = async (req, res) => {
+
+    const { id } = req.params;
+
+    const proyecto = await Proyecto.findById(id.trim());
+
+    // ? En caso de no exista el proyecto se muestra un msg
+    if (!proyecto && id.length === 24) {
+        const error = new Error('El Proyecto no Existe')
+        return res.status(404).json({ msg: error.message })
+    }
+
+    // ? Cuando el usuario no tiene permisos para ver el proyecto
+    if (proyecto.creador.toString() !== req.usuario._id.toString()) {
+        const error = new Error('Acción no Valida')
+        return res.status(401).json({ msg: error.message })
+    }
+
+    // ? Obtenemos los datos para actualizar y validamos en caso de que no exista
+    proyecto.nombre = req.body.nombre || proyecto.nombre;
+    proyecto.descripcion = req.body.descripcion || proyecto.descripcion;
+    proyecto.fechaEntrega = req.body.fechaEntrega || proyecto.fechaEntrega;
+    proyecto.cliente = req.body.cliente || proyecto.cliente;
+
+    try {
+        const proyectoAlmacenado = await proyecto.save();
+        res.json(proyectoAlmacenado)
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+// * Eliminar proyecto
+const eliminarProyecto = async (req, res) => {
+
+    const { id } = req.params;
+
+    const proyecto = await Proyecto.findById(id.trim());
+
+    // ? En caso de no exista el proyecto se muestra un msg
+    if (!proyecto && id.length === 24) {
+        const error = new Error('El Proyecto no Existe')
+        return res.status(404).json({ msg: error.message })
+    }
+
+    // ? Cuando el usuario no tiene permisos para ver el proyecto
+    if (proyecto.creador.toString() !== req.usuario._id.toString()) {
+        const error = new Error('Acción no Valida')
+        return res.status(401).json({ msg: error.message })
+    }
+
+    try {
+        await proyecto.deleteOne();
+        res.json({ msg: "Proyecto Eliminado "});
+    } catch (error) {
+        console.log(error)
+    }
+}
+const agregarColaborador = async (req, res) => { }
+const eliminarColaborador = async (req, res) => { }
+const obtenerTareas = async (req, res) => { }
+
+export {
     obtenerProyectos,
     nuevoProyecto,
     obtenerProyecto,
