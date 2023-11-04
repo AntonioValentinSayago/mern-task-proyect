83bd0bd6345bf570ceb664666386cca4d09c5074
AntonioValentin <cesar_anto30@hotmail.com>
2023-11-04 17:36:06 -0600

    Nuevo Modelo para las Tareas

---
+++ backend/models/Tarea.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose"; 
+
+const tareaShema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    descripcion:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    estado: {
+        type: Boolean,
+        default: false,
+    },
+    fechaEntrega:{
+        type: Date,
+        required: true,
+        default: Date.now()
+    },
+    prioridad: {
+        type: String,
+        required: true,
+        enum: ["Baja", "Media", "Alta"]
+    },
+    proyecto:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Proyecto",
+    }
+},{
+    timestamps: true
+})
+
+const Tarea = mongoose.model("Tareas", tareaShema);
+
+export default Tarea;
