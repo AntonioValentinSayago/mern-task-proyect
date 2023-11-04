ffa0b4bf246e45bc4870129acc6dfc45dc6c7878
AntonioValentin <cesar_anto30@hotmail.com>
2023-11-04 17:01:29 -0600

    Funcion para obtener Proyecto (Validar el BUG de conversion toString() )

--- backend/controllers/proyectoController.js
+++ backend/controllers/proyectoController.js
@@ -20,22 +20,34 @@ const nuevoProyecto = async ( req, res ) => {
     }
 
 }
-const obtenerProyecto = async ( req, res ) => {}
-const editarProyecto = async ( req, res ) => {
+
+// * Obtener proyecto existente desde la url
+const obtenerProyecto = async ( req, res ) => {
+
+    const { id } = req.params;
+
+    const proyecto = await Proyecto.findById(id.trim());
+
+    // ? En caso de no exista el proyecto se muestra un msg
+    if(!proyecto && id.length === 24){
+        const error = new Error('El Proyecto no Existe')
+        return res.status(404).json({ msg: error.message })
+    }
+    
+    // ? Cuando el usuario no tiene permisos para ver el proyecto
+    if(proyecto.creador.toString() !== req.usuario._id.toString()){
+        const error = new Error('Acción no Valida')
+        return res.status(401).json({ msg: error.message })
+    }
+
+    res.json(proyecto)
 
 }
-const eliminarProyecto = async ( req, res ) => {
-
-}
-const agregarColaborador = async ( req, res ) => {
-
-}
-const eliminarColaborador = async ( req, res ) => {
-
-}
-const obtenerTareas = async ( req, res ) => {
-
-}
+const editarProyecto = async ( req, res ) => {}
+const eliminarProyecto = async ( req, res ) => {}
+const agregarColaborador = async ( req, res ) => {}
+const eliminarColaborador = async ( req, res ) => {}
+const obtenerTareas = async ( req, res ) => {}
 
 export{
     obtenerProyectos,
