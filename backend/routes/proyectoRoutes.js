48c9fc33245c18e010d31332e72aa1281faad69a
AntonioValentin <cesar_anto30@hotmail.com>
2023-11-06 17:24:52 -0600

    Actualizacion del Metodo para Obtener Tarea por proyecto

--- backend/routes/proyectoRoutes.js
+++ backend/routes/proyectoRoutes.js
@@ -7,8 +7,7 @@ import {
     editarProyecto,
     eliminarProyecto,
     agregarColaborador,
-    eliminarColaborador,
-    obtenerTareas
+    eliminarColaborador
 } from "../controllers/proyectoController.js"
 import checkAuth from "../middleware/checkAuth.js"
 
@@ -21,7 +20,7 @@ router.route('/:id')
     .put(checkAuth, editarProyecto)
     .delete(checkAuth, eliminarProyecto);
 
-router.get('/tareas/:id', checkAuth, obtenerTareas)
+// router.get('/tareas/:id', checkAuth, obtenerTareas)
 router.post('/agregar-colaborador/:id', checkAuth, agregarColaborador)
 router.post('/eliminar-colaborador/:id', checkAuth, eliminarColaborador)
 
