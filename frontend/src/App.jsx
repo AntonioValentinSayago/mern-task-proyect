01bb62c74c989e3d414abc55671e098d7bc7f56f
AntonioValentin <cesar_anto30@hotmail.com>
2023-11-07 21:43:47 -0600

    Configuracion de las Rutas para la navegacion del sitio

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,27 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import AuthLayout from './layouts/AuthLayout'
+
+import Login from './pages/Login'
+import Registrar from './pages/Registrar'
+import OlvidePassword from './pages/OlvidePassword'
+import NuevoPassword from './pages/NuevoPassword'
+import ConfirmarCuneta from './pages/ConfirmarCuneta'
 
 function App() {
   
   return (
-    <>
-      <h1 className="text-blue-700">Hola</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<AuthLayout/>}>
+          <Route index element={<Login />} />
+          <Route path='registrar' element={<Registrar />} />
+          <Route path='olvide-password' element={<OlvidePassword />} />
+          <Route path='olvide-password/:token' element={<NuevoPassword />} />
+          <Route path='confirmar/:id' element={<ConfirmarCuneta />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
