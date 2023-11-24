16defcc7362b7595b8c4f2a0d90a9d4f9be12cc9
AntonioValentin <cesar_anto30@hotmail.com>
2023-11-23 18:10:43 -0600

    Importacion del Token para la validacion si el usuario esta autenticado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,19 +8,31 @@ import OlvidePassword from './pages/OlvidePassword'
 import NuevoPassword from './pages/NuevoPassword'
 import ConfirmarCuneta from './pages/ConfirmarCuneta'
 
+
+import { AuthProvider } from './context/AuthProvider'
+import RutaProtegida from './layouts/RutaProtegida'
+import Proyectos from './pages/Proyectos'
 function App() {
-  
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<AuthLayout/>}>
-          <Route index element={<Login />} />
-          <Route path='registrar' element={<Registrar />} />
-          <Route path='olvide-password' element={<OlvidePassword />} />
-          <Route path='olvide-password/:token' element={<NuevoPassword />} />
-          <Route path='confirmar/:id' element={<ConfirmarCuneta />} />
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path='/' element={<AuthLayout />}>
+            <Route index element={<Login />} />
+            <Route path='registrar' element={<Registrar />} />
+            <Route path='olvide-password' element={<OlvidePassword />} />
+            <Route path='olvide-password/:token' element={<NuevoPassword />} />
+            <Route path='confirmar/:id' element={<ConfirmarCuneta />} />
+          </Route>
+
+          <Route path='/proyectos' element={<RutaProtegida />}>
+            <Route index element={<Proyectos />} />
+          </Route>
+
+        </Routes>
+      </AuthProvider>
+
     </BrowserRouter>
   )
 }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,55 +1,109 @@
-import { Link } from "react-router-dom"
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import Alerta from "../components/Alerta"
+
+import useAuth from "../hooks/useAuth"
+import axios from "axios";
 
 const Login = () => {
+
+  // ? Validacion del useSate para el usuario
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [alerta, setAlerta] = useState({});
+
+  const { setAuth } = useAuth();
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    // Validar los campos del formulario
+    if ([email, password].includes('')) {
+      setAlerta({
+        msg: 'Los campos son Obligatorios',
+        error: true
+      });
+      return;
+    }
+
+    // Todo: Se envia información al Servidor
+    try {
+      const { data } = await axios.post('http://localhost:4000/api/usuarios/login', { email, password })
+
+      setAlerta({})
+
+      // ? almacenar el token del usuario autenticado en Local Storage
+      localStorage.setItem('token', data.token)
+      setAuth(data);
+
+    } catch (error) {
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true
+      })
+    }
+
+  }
+  const { msg } = alerta;
   return (
     <>
       <h1 className="text-sky-600 font-black text-4xl capitalize">
         Inicia Sesión y Administra tus <span className="text-slate-700">Proyectos</span>
       </h1>
-      <form className="my-10 bg-white shadow rounded-lg px-10 py-5">
+
+      {msg && <Alerta alerta={alerta} />}
+
+      <form
+        className="my-10 bg-white shadow rounded-lg px-10 py-5"
+        onSubmit={handleSubmit}
+      >
         <div className="my-5">
-          <label 
+          <label
             className="uppercase text-gray-600 block text-xl font-bold"
             htmlFor="email"
           > Email</label>
-          <input 
-            type="email" 
+          <input
+            id="email"
+            type="email"
             placeholder="Email de Registro"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
-            id="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
           />
         </div>
 
         <div className="my-5">
-          <label 
+          <label
             className="uppercase text-gray-600 block text-xl font-bold"
             htmlFor="password"
           > Password</label>
-          <input 
-            type="password" 
+          <input
+            id="password"
+            type="password"
             placeholder="Password de Registro"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
-            id="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
           />
         </div>
 
-        <input 
-          type="submit" 
-          value="Iniciar Sesion" 
+        <input
+          type="submit"
+          value="Iniciar Sesion"
           className="bg-sky-700 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors" />
 
       </form>
 
-    <nav className="lg:flex lg:justify-between">
-      <Link 
-        className="block text-center my-5 text-slate-500 uppercase text-sm"
-        to="registrar"
-      >¿No tienes una Cuenta? Registrate</Link>
-      <Link 
-        className="block text-center my-5 text-slate-500 uppercase text-sm"
-        to="olvide-password"
-      >Olvide mi Password</Link>
-    </nav>
+      <nav className="lg:flex lg:justify-between">
+        <Link
+          className="block text-center my-5 text-slate-500 uppercase text-sm"
+          to="registrar"
+        >¿No tienes una Cuenta? Registrate</Link>
+        <Link
+          className="block text-center my-5 text-slate-500 uppercase text-sm"
+          to="olvide-password"
+        >Olvide mi Password</Link>
+      </nav>
 
     </>
   )
