62688eb82668c9d7f56181efa7917f32eed49821
AntonioValentin <cesar_anto30@hotmail.com>
2024-02-08 12:31:42 -0600

    Implementacion de la funcion 'Eliminar Proyecto'

--- frontend/src/context/ProyectosContext.jsx
+++ frontend/src/context/ProyectosContext.jsx
@@ -156,6 +156,40 @@ const ProyectosProvider = ({ children }) => {
         }
     }
 
+    //* Funcion para eliminar el proyecto
+    const eliminarProyecto = async id => {
+        try {
+            const token = localStorage.getItem('token');
+            if (!token) return;
+
+            const config = {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`
+                }
+            }
+
+            const { data } = await clienteAxios.delete(`http://localhost:4000/api/proyectos/${id}`,config);
+
+            //* Sincronizar los Proyectos en el state
+            const proyectosActualizados = proyectos.filter(proyectoState => proyectoState._id !== id)
+            setProyectos(proyectosActualizados)
+
+            setAlerta({
+                msg: data.msg,
+                error: false
+            })
+
+            setTimeout(() => {
+                setAlerta({})
+                navigate('/proyectos')
+            }, 3000);
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <ProyectosContext.Provider
             value={{
@@ -165,7 +199,8 @@ const ProyectosProvider = ({ children }) => {
                 submitProyecto,
                 obtenerProyecto,
                 proyecto,
-                cargando
+                cargando,
+                eliminarProyecto
             }}
         >
             {children}
