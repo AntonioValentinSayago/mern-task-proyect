6d0b581d6876914b7d4eb33f0b29d46948cc78c5
AntonioValentin <cesar_anto30@hotmail.com>
2024-02-09 12:45:16 -0600

    Implementacion de la Vista para mostrar las tareas

--- frontend/src/pages/Proyecto.jsx
+++ frontend/src/pages/Proyecto.jsx
@@ -1,36 +1,82 @@
-import { useParams,Link } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import useProyectos from '../hooks/useProyectos';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+//* Importacion del Modal
+import ModalFormularioTarea from '../components/ModalFormularioTarea';
+import Tarea from '../components/Tarea';
 
 const Proyecto = () => {
 
     const params = useParams();
-    const { obtenerProyecto, proyecto, cargando } = useProyectos();
+    const { obtenerProyecto, proyecto, cargando, handleModalTarea } = useProyectos();
+
+    //* State para Maneja el modal
+    const [ modal, setModal ] = useState(false);
 
     useEffect(() => {
         obtenerProyecto(params.id)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     const { nombre } = proyecto;
 
     return (
         cargando ? 'Cargando...' : (
-            <div className='flex justify-between'>
-                <h1 className="font-black text-4xl">{nombre}</h1>
+            <>
 
-                <div className='flex items-center gap-2 text-gray-400 hover:text-black cursor-pointer'>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+                <div className='flex justify-between'>
+                    <h1 className="font-black text-4xl">{nombre}</h1>
+
+                    <div className='flex items-center gap-2 text-gray-400 hover:text-black cursor-pointer'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+                        </svg>
+
+                        <Link
+                            to={`/proyectos/editar/${params.id}`}
+                            className='uppercase font-bold'
+                        >
+                            Editar
+                        </Link>
+                    </div>
+
+                </div>
+                <button
+                    onClick={ handleModalTarea }
+                    type='button'
+                    className='text-sm px-5 py-3 w-full md:w-auto rounded-lg uppercase font-bold bg-sky-400 text-white text-center mt-5 flex gap-2 items-center justify-center'
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                     </svg>
 
-                    <Link
-                        to={`/proyectos/editar/${params.id}`}
-                        className='uppercase font-bold'
-                    >
-                        Editar
-                    </Link>
+                    Nueva Tarea
+                </button>
+                
+                <p 
+                    className="font-bold text-xl mt-10"
+                >
+                    Tareas del Proyecto
+                </p>
+
+                <div className="bg-white shadow mt-10 rounded-lg">
+                    { proyecto.tareas?.length ? 
+                        proyecto.tareas?.map( tarea => (
+                            <Tarea 
+                                key={tarea._id}
+                                tarea={tarea}
+                            />
+                        )) : 
+                        <p className='text-center my-5 p-10'>No hay Tareas en este Proyecto</p>
+                    }
                 </div>
-            </div>
+
+                <ModalFormularioTarea 
+                    modal={modal}
+                    setModal={setModal}
+                />
+            </>
         )
     )
 }
