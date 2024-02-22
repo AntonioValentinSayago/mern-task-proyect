23f230436aea601a9db63b8df3ea6aa9ea1fff54
AntonioValentin <cesar_anto30@hotmail.com>
2024-02-21 20:12:32 -0600

    Configuracion de Nueva Ruta - 'NuevoColaborador'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,6 +17,7 @@ import Proyectos from './pages/Proyectos'
 import { NuevoProyecto } from './pages/NuevoProyecto'
 import  Proyecto  from './pages/Proyecto'
 import  EditarProyecto  from './pages/EditarProyecto'
+import NuevoColaborador from './pages/NuevoColaborador'
 
 
 function App() {
@@ -37,6 +38,7 @@ function App() {
             <Route path='/proyectos' element={<RutaProtegida />}>
               <Route index element={<Proyectos />} />
               <Route path='crear-proyecto' element={<NuevoProyecto />} />
+              <Route path='nuevo-colaborador/:id' element={<NuevoColaborador />} />
               <Route path=':id' element={<Proyecto />} />
               <Route path='editar/:id' element={<EditarProyecto />} />
             </Route>
