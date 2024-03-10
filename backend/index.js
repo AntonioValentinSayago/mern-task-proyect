b28eb966c00f53252de55c19b384f1e7560a8101
AntonioValentin <cesar_anto30@hotmail.com>
2024-03-09 23:53:55 -0600

    Correcion del BUG de los CORS v.1.0.0

--- backend/index.js
+++ backend/index.js
@@ -14,21 +14,22 @@ dotenv.config();
 
 conectarDB();
 
-// ! Configuracion del CORS
-const whiteList = ["http://localhost:5173"];
+// Configurar CORS
+const whitelist = [process.env.FRONTEND_URL];
 
 const corsOptions = {
-    origin: function (origin, callback) {
-        if (whiteList.includes(origin)) {
-            // ? Puede consultar la API
-            callback(null, true)
-        } else {
-            // ? No esta permitido
-            callback(new Error("Error de Cors"))
-        }
+  origin: function (origin, callback) {
+    if (whitelist.includes(origin)) {
+      // Puede consultar la API
+      callback(null, true);
+    } else {
+      // No esta permitido
+      callback(new Error("Error de Cors"));
     }
-}
-app.use(cors(corsOptions))
+  },
+};
+
+app.use(cors(corsOptions));
 
 
 // ? Routing -> Definir las rutas
