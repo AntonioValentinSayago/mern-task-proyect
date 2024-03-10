880ef52e9acc156adab8f03710890978b895e619
AntonioValentin <cesar_anto30@hotmail.com>
2024-03-09 23:57:02 -0600

    Formatear el Codigo del documento

--- frontend/src/pages/NuevoColaborador.jsx
+++ frontend/src/pages/NuevoColaborador.jsx
@@ -1,20 +1,24 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from "react"
 import FormularioColaborador from "./FormularioColaborador"
 import useProyectos from "../hooks/useProyectos"
 
 import { useParams } from "react-router-dom" 
+import Alerta from "../components/Alerta"
 
 const NuevoColaborador = () => {
 
-    const { obtenerProyecto, proyecto, cargando } = useProyectos()
+    const { obtenerProyecto, proyecto, cargando, colaborador, agregarColaborador, alerta } = useProyectos()
     const paramas = useParams();
 
     useEffect(() => {
         obtenerProyecto(paramas.id)
     },[])
 
-    if(cargando) return 'Cargando...'
+    //if(cargando) return 'Cargando...'
 
+    // * Validando que exista un proyecto
+    if(!proyecto?._id) return <Alerta alerta={alerta} />
   return (
     <>
         <h1 className="text-4xl font-black">Añadir Colaborador (a) al Proyecto: {proyecto.nombre}</h1>
@@ -22,6 +26,26 @@ const NuevoColaborador = () => {
         <div className="mt-10 flex justify-center">
             <FormularioColaborador />
         </div>
+
+        {cargando ? <p className="text-center">cargando...</p> : colaborador?._id && (
+        <div className='flex justify-center mt-10'>
+            <div className='bg-white py-10 px-5 md:w-1/2 rounded-lg shadow w-full'>
+                <h2 className='text-center mb-10 text-2xl font-bold'>Resultado:</h2>
+
+                <div className='flex justify-between items-center'>
+                  <p>{colaborador.nombre}</p>
+
+                  <button
+                    type="button"
+                    className='bg-slate-500 px-5 py-2 rounded-lg uppercase text-white font-bold text-sm'
+                    onClick={() => agregarColaborador({
+                      email: colaborador.email
+                    })}
+                  >Agregar al Proyecto</button>
+                </div>
+            </div>
+        </div>
+      ) }
     </>
   )
 }
