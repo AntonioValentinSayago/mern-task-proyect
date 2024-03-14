c2533b3936ec086b2d9aa9962f988fe02b2913e9
AntonioValentin <cesar_anto30@hotmail.com>
2024-03-13 23:23:56 -0600

    Se actualiza la leyenda de colaborador

--- frontend/src/components/PreviewProyecto.jsx
+++ frontend/src/components/PreviewProyecto.jsx
@@ -1,26 +1,35 @@
 import { Link } from "react-router-dom"
-
+import useAuth from "../hooks/useAuth"
 // eslint-disable-next-line react/prop-types
 const PreviewProyecto = ({ proyecto }) => {
-    // eslint-disable-next-line react/prop-types
-    const { nombre, _id, cliente } = proyecto
+
+  const { auth } = useAuth()
+  // eslint-disable-next-line react/prop-types
+  const { nombre, _id, cliente, creador } = proyecto
   return (
     <div
       className="border-b p-5 flex"
     >
-      <p
-        className="flex-1"
-      >
-        {nombre}
+      <div className='flex items-center gap-2'>
+        <p className='flex-1'>
+          {nombre}
 
-        <span className="text-sm text-gray-500 uppercase">{''} {cliente}</span>
-      </p>
+          <span className='text-sm text-gray-500 uppercase'>
+            {''} {cliente}
+          </span>
+        </p>
+
+        {auth._id !== creador && (
+          <p className='p-1 text-xs rounded-lg text-white bg-green-500 font-bold uppercase'>Colaborador</p>
+        )}
+      </div>
+
 
       <Link
         className="text-green-600 hover:text-gray-800 uppercase text-sm font-bold"
         to={`${_id}`}
       >
-      Ver Proyecto</Link>
+        Ver Proyecto</Link>
     </div>
   )
 }
